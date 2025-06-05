fa0056708f767399a70375e44e30fa531b4a733b
fallxn <axlsntz.dev@gmail.com>
2025-06-04 23:24:40 -0600

    feat: Add new components and pages for JoiSolutions
    - Implemented the CTO component to highlight company information and contact options.
    - Created a Footer component with navigation links and legal information.
    - Developed the HeroJoiPay component to showcase the JoiPay product with engaging visuals.
    - Added Mision and Purpose sections to articulate the company's mission and vision.
    - Introduced QuoteAnimation component for dynamic text display.
    - Built Solutions component to present product offerings with visual elements.
    - Designed various icon components for branding and product representation.
    - Enhanced the LayoutJoiPay for a cohesive structure across JoiPay pages.
    - Updated the productos index page to display product details and links.
    - Created a dedicated page for JoiPay with a hero section for focused marketing.

---
+++ src/assets/scripts/lenis.js
@@ -0,0 +1,18 @@
+import Lenis from  "lenis"
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// Register the ScrollTrigger plugin with GSAP
+gsap.registerPlugin(ScrollTrigger);
+// Initialize a new Lenis instance for smooth scrolling
+const lenis = new Lenis();
+
+// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+lenis.on('scroll', ScrollTrigger.update);
+
+// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+});
+
+
